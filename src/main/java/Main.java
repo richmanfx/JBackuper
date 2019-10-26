14ce9e9e3f5b4e9edba50da3f7695e549b24cef3
richmanfx <richmanfx@yandex.ru>
2019-10-26 14:19:02 +0300

    Task "run" для Gradle. Логирование.

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final Logger log = LogManager.getLogger();
+        log.info("=--> Start JBackuper");
+
+    }
+}
